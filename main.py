51f18336e3e53aa179e1963b256efd01ede605b7
Leo Tran <62197870+sgtbonsai@users.noreply.github.com>
2022-03-13 23:13:13 +0000

    spicy

--- main.py
+++ main.py
@@ -2,7 +2,7 @@
 from datetime import datetime
 from pytz import timezone
 
-fmt = "%Y/%m/%d %I:%M:%S"
+fmt = "%m/%d/%Y %I:%M:%S"
 timezonelist = ['Zulu','US/Central']
 for zone in timezonelist:
     now_time = datetime.now(timezone(zone))
